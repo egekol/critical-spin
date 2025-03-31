ab9d00c225ae8ab6381ac75adbeb21d30faaa0a5
egekol@hotmail.com <papercut>
2025-04-01 00:13:41 +0300

    [Added] Icon Atlas and constants

---
+++ Assets/Main/Scripts/Utilities/CardGameConstants.cs
@@ -0,0 +1,8 @@
+namespace Main.Scripts.Utilities
+{
+    public static class CardGameConstants
+    {
+        public const string RewardViewModelPrefix = "so_reward_";
+        public const string RewardViewModelIconPrefix = "ui_icon_";
+    }
+}
