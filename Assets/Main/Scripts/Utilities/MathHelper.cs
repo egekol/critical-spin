822bc7c0840af52d56ce737f2fb1b29494ee1cba
egekol@hotmail.com <papercut>
2025-04-01 00:11:56 +0300

    [Added] MathHelper

---
+++ Assets/Main/Scripts/Utilities/MathHelper.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+namespace Main.Scripts.Utilities
+{
+    public static class MathHelper
+    {
+        private static readonly Random _random = new Random();
+
+        public static float Map(float value, float inMin, float inMax, float outMin, float outMax)
+        {
+            return outMin + (value - inMin) * (outMax - outMin) / (inMax - inMin);
+        }
+        public static float Map(int value, float inMin, float inMax, float outMin, float outMax)
+        {
+            return outMin + (value - inMin) * (outMax - outMin) / (inMax - inMin);
+        }
+        public static int GetRandomIndex<T>(List<T> list)
+        {
+            return _random.Next(0, list.Count); // Returns a random index in range [0, list.Count - 1]
+        }
+        public static int GetRandomIndex<T>(T[] array)
+        {
+            return _random.Next(0, array.Length); // Returns a random index in range [0, array.Length - 1]
+        }
+    }
+}
